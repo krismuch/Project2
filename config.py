b619da1395b6e88291fdf4f2645810a447507e5f
Nand Kishore Sagi <NandKishore.Sagi@gmail.com>
2019-09-19 17:26:13 -0400

    Updates

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    "page_size":50,
+    "token":"MUVzYkNhNDYraDBGcnJPVG5ZckRaZz09"
+}
+
+dbConfig={
+    "user":"postgres",
+    "password":"postgres",
+    "host":"127.0.0.1",
+    "port":"5432",
+    "database":"data_farm"
+}
