e491f670d22193ef0ae10be103935a46e0d87936
Kristen Mason <krmuch@gmail.com>
2019-09-24 20:56:24 -0400

    updated app.py

--- app.py
+++ app.py
@@ -13,26 +13,6 @@ from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 #################################################
-# Database Setup
-#################################################
-
-#try:
-#    engine = create_engine("sqlite:///data_farming.db")
-#   print("connectedtosqlitedb")
-#except expression as identifier:
-#    print(identifier)
-
-
-# reflect an existing database into a new model
-#Base = automap_base()
-# reflect the tables
-#Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-#NC_MONTHLY_WEATHER = Base.classes.NC_MONTHLY_WEATHER
-#PLANT_CHARACTERISTICS = Base.classes.PLANT_CHARACTERISTICS
-
-#################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
@@ -51,7 +31,7 @@ def names():
 
     with sql.connect("db//data_farming.db") as con:
         cur = con.cursor()
-        cur.execute("select distinct common_name from PLANT_CHARACTERISTICS")
+        cur.execute("select distinct UPPER(common_name) from PLANT_CHARACTERISTICS WHERE common_name is NOT NULL ORDER BY common_name")
 
         rows = cur.fetchall()
  
@@ -59,9 +39,45 @@ def names():
             print(row)
 
         return jsonify(rows)
-                
-        #con.commit()
-        #msg = "Record successfully added"
+
+@app.route("/weather/<year_>/<month_>")
+def weather(year_, month_):
+    """Return a list of distinct plant names."""
+    
+    with sql.connect("db//data_farming.db") as con:
+        cur = con.cursor()
+        cur.execute(f"SELECT c.county, w.tmax, w.tmin, w.prcp, w.year, w.month FROM NC_COUNTIES AS c INNER JOIN NC_MONTHLY_WEATHER AS w ON w.FIPS = c.FIPS WHERE year={year_} and month={month_}")
+        
+        rows = cur.fetchall()
+ 
+        for row in rows:
+            print(row)
+
+        return jsonify(rows)
+
+@app.route("/metadata/<plant>")
+def plant_metadata(plant):
+    """Return the MetaData for a given plant."""
+    print(plant)
+    with sql.connect("db//data_farming.db") as con:
+        cur = con.cursor()
+        cur.execute(f"SELECT common_name, min_temp_deg_f, frost_free_days, min_precip_inches, max_precip_inches, drought_tolerance FROM PLANT_CHARACTERISTICS WHERE UPPER(common_name)='{plant.upper()}'")
+
+        results = cur.fetchall()
+
+    # Create a dictionary entry for each row of metadata information
+    plant_metadata = {}
+    for result in results:
+        plant_metadata["common_name"] = result[0]
+        plant_metadata["min_temp_deg_f"] = result[1]
+        plant_metadata["frost_free_days"] = result[2]
+        plant_metadata["min_precip_inches"] = result[3]
+        plant_metadata["max_precip_inches"] = result[4]
+        plant_metadata["drought_tolerance"] = result[5]
+
+
+    print(plant_metadata)
+    return jsonify(plant_metadata)
 
 if __name__ == "__main__":
     app.run()
